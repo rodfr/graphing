caa9d6207c80aa61a338aebce7cd83817bafc90a
Francisco R. G <rodriguezgomez.fj@gmail.com>
2015-04-30 00:28:59 +0200

    split code into main.cpp and class.cpp;

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+// main.cpp
+// by frg
+#include "graph.cpp"
+
+//////////////////////////////////////////////////
+// launch main prog
+//////////////////////////////////////////////////
+
+//int main(int argc, char* argv[])
+int main()
+{
+  cout<< "debut main"<<endl;
+
+  Graph* myGraph = new Graph();
+
+  Node a;
+  cout <<a<< endl;
+  a.setLabel("a");
+  Node b("b");
+  Node c("c");
+  Node d("d");
+  Node e("e");
+
+  vector<Node*> someVector;
+
+  someVector.push_back(&b);
+  someVector.push_back(&c);
+  someVector.push_back(&d);
+
+  a.add2Neighs(someVector );
+  c.add2Neighs(&d);
+  
+  cout<< a <<endl;
+  cout<< b <<endl;
+
+  myGraph->addNode(&a); 
+  myGraph->addNode(&b); 
+  myGraph->addNode(&c); 
+  myGraph->addNode(&d); 
+  myGraph->addNode(&e);
+
+  cout<<"\n his is the new graph: "<< (*myGraph)<<endl;
+
+  vector<Node*> sortedNodes = myGraph->BFS();
+
+  for (vector<Node*>::iterator it =sortedNodes.begin();it!=sortedNodes.end();it++)
+  {
+    cout<< (*it)->getLabel()<<" | ";
+  } 
+
+  cout<< "outtahere"<<endl;
+}
